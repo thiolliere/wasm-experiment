7a2a9f8a36c92932497b4341e1b48ce04c24936c
thiolliere <gui.thiolliere@gmail.com>
2017-12-13 15:40:36 +0100

    winit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 #![recursion_limit="500"]
 #[macro_use] extern crate stdweb;
+extern crate winit;
+
+use stdweb::unstable::TryInto;
 use std::os::raw::c_void;
 use std::ptr;
 use std::cell::RefCell;
 
 mod ffi;
+mod animation;
 
-// Used to assign a callback to emscripten main loop
 thread_local!(static MAIN_LOOP_CALLBACK: RefCell<*mut c_void> = RefCell::new(ptr::null_mut()));
 
-// Used to assign a callback to emscripten main loop
 fn set_main_loop_callback<F>(callback : F) where F : FnMut() {
     MAIN_LOOP_CALLBACK.with(|log| {
         *log.borrow_mut() = &callback as *const _ as *mut c_void;
@@ -98,7 +100,25 @@ pub struct Camera {
 pub fn main() {
     stdweb::initialize();
     let mut graphics = Graphics::initialize();
+    let mut events_loop = winit::EventsLoop::new();
     set_main_loop_callback(|| {
+        events_loop.poll_events(|event| {
+            match event {
+                winit::Event::WindowEvent {
+                    event: winit::WindowEvent::Touch(mut touch), ..
+                } => {
+                    let height: f64 = js! {return tgl.canvas.height}.try_into().unwrap();
+                    let width: f64 = js! {return tgl.canvas.width}.try_into().unwrap();
+
+                    touch.location.0 -= width/2.0;
+                    touch.location.0 /= width/2.0;
+                    touch.location.1 -= height/2.0;
+                    touch.location.1 /= height/2.0;
+                    touch.location.1 *= -1.;
+                },
+                _ => (),
+            }
+        });
         graphics.insert_draw(0, 100.0, 100.0, 0.5, Layer::Ceil);
         graphics.draw(&Camera {
             zoom: 100.0,
